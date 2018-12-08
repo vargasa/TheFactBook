548a9307499420a4b7802522f1587e065ccac202
Andres Vargas <vargasa@cua.edu>
2018-12-08 23:57:35 +0100

    Improve Readability

--- ChromeExtension/TFBMain.js
+++ ChromeExtension/TFBMain.js
@@ -5,18 +5,49 @@ chrome.browserAction.setBadgeText({
     text: "TFB"
 });
 
+function notifyUser(message){
+    chrome.notifications.create("basic", {
+        "type":"basic",
+        "iconUrl":"Data/TFBIcon.png",
+        "title":"TheFactBook:",
+        "message": message.toString()
+    });
+}
+
 //--------------------------------------------------------------------------//
+
+function sendFact(fact, source, tags, tfbSettings) {
+    var http = new XMLHttpRequest();
+    http.open("POST", tfbSettings.url + "/api.php?action=addFact", true);
+    http.setRequestHeader("Content-type",
+                          "application/x-www-form-urlencoded");
+    http.timeout = tfbSettings.timeout * 1000;
+    http.ontimeout = function() {
+        notifyUser("TheFactBook server has Timed out");
+    };
+    http.onreadystatechange = function () {
+        if (  this.readyState == 4 && this.status == 200) {
+            var r = http.responseText;
+            r = JSON.parse(r);
+            if (! r.Error){
+                notifyUser("Your Fact has been saved!");
+            } else {
+                notifyUser(r.Description);
+            }
+        }
+    };
+    tfbUsername = encodeURIComponent(tfbSettings.username);
+    tfbPassword = encodeURIComponent(tfbSettings.password);
+    var params = "username=" + tfbUsername
+        + "&password=" + tfbPassword
+        + "&fact=" + fact
+        + "&source=" + source
+        + "&tags=" + tags;
+    http.send(params);
+}
 
 var clickHandler = function(e) {
 
-    function notifyUser(message){
-	chrome.notifications.create("basic", {
-	    "type":"basic",
-	    "iconUrl":"Data/TFBIcon.png",
-	    "title":"TheFactBook:",
-	    "message": message.toString()
-	});
-    }
 
     //--------------------------------------------------------------------------//
 
@@ -27,66 +58,32 @@ var clickHandler = function(e) {
 	tfbPassword: 'guest',
 	tfbEditBeforeSend: true
     };
-    
+
     chrome.storage.sync.get(tfbDefaultOptions, getAsyncOptions);
 
     //--------------------------------------------------------------------------//
 
     function getAsyncOptions(items) {
-	var tfbUrl = items.tfbUrl;
-	var tfbTimeout = items.tfbTimeout;
-	var tfbUsername = items.tfbUsername;
-	var tfbPassword = items.tfbPassword;
-	var tfbEditBeforeSend = items.tfbEditBeforeSend;
 
+	var tfbSettings = {
+	    url : items.tfbUrl,
+	    timeout : items.tfbTimeout,
+	    username : items.tfbUsername,
+	    password : items.tfbPassword,
+	    editBeforeSend : items.tfbEditBeforeSend
+	}
 	//--------------------------------------------------------------------------//
 	function getAsyncTabUrl(tabs) {
 	    tabUrl = tabs[0].url;
-	    sendRequest();
+	    sendRequest(tabUrl);
 	}
-	
+
 	chrome.tabs.query({currentWindow: true, active: true}, getAsyncTabUrl);
 	//--------------------------------------------------------------------------//
 
-	function sendRequest() {
+	function sendRequest(tabUrl) {
 
-	    function sendFact(fact, source, tags) {
-		var http = new XMLHttpRequest();
-		http.open("POST", tfbUrl + "/api.php?action=addFact", true);
-		http.setRequestHeader("Content-type", 
-				      "application/x-www-form-urlencoded");
-		
-		http.timeout = tfbTimeout * 1000;
-		http.ontimeout = function() { 
-		    notifyUser("TheFactBook server has Timed out"); 
-		};
-		
-		
-		http.onreadystatechange = function () {
-		    if (  this.readyState == 4 && this.status == 200) {
-			var r = http.responseText;
-			r = JSON.parse(r);
-			if (! r.Error){
-			    notifyUser("Your Fact has been saved!");
-			} else {
-			    notifyUser(r.Description);
-			}
-		    }
-		};
-		
-		tfbUsername = encodeURIComponent(tfbUsername);
-		tfbPassword = encodeURIComponent(tfbPassword);
-		var params = "username=" + tfbUsername
-		    + "&password=" + tfbPassword
-		    + "&fact=" + fact
-		    + "&source=" + source
-		    + "&tags=" + tags;
-		
-		http.send(params);
-		
-	    }
-	    
-	    if (tfbEditBeforeSend) {
+	    if (tfbSettings.editBeforeSend) {
 		chrome.tabs.create({
 		    url: chrome.extension.getURL('TFBEditFact.html'),
 		    active: true,
@@ -108,7 +105,7 @@ var clickHandler = function(e) {
 		    chrome.runtime.onMessage.addListener(
 			function(request, sender, sendResponse){
 			    if (request.msg == "sendFact"){
-				sendFact(request.fact, request.source, request.tags);
+				sendFact(request.fact, request.source, request.tags, tfbSettings);
 				
 				if ( request.dontEdit ){
 				    chrome.storage.sync.set({ "tfbEditBeforeSend": false });
@@ -123,7 +120,7 @@ var clickHandler = function(e) {
 		
 
 	    } else {
-		sendFact(e.selectionText, tabUrl, "");
+		sendFact(e.selectionText, tabUrl, "", tfbSettings);
 	    }
 	}
     }	
