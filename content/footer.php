4885f72728f4d450b276482b3207f0528749420a
Andres Vargas <vargasa@cua.edu>
2019-01-16 21:05:34 -0500

    Add link to Chrome Store Extension download

--- content/footer.php
+++ content/footer.php
@@ -5,7 +5,7 @@
       [<a href="tos.php#tos">Terms of Service</a>]
       [<a href="tos.php#privacy">Privacy Policy</a>]
       [<a href="content/TFBAddon.xpi">Firefox Addon</a>]
-      [<a href="content/TFBExtension.crx">Chrome Extenssion</a>]
+      [<a href="https://chrome.google.com/webstore/detail/thefactbook-addon/dpbilhlppmkhihilemffdhbmbmhcbnmk">Chrome Extenssion</a>]
       [<a href="http://www.k-lab.tk" target="_blank">It's a K-Lab Project</a>]
     </div>
   </div>
